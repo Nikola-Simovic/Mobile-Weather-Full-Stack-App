90a7b45f635ed4db52459f46fdb1ed00afd17a5b
Nikola <nikola.simovic@tuni.fi>
2024-04-26 12:11:24 +0300

    Created a version with scaffolding top the top and bottom in preparation for the final App version, removed the navigation from it for now though.

---
+++ app/src/main/java/com/example/myapplication/CurrentWeatherScreenTEST.kt
@@ -0,0 +1,127 @@
+package com.example.myapplication
+
+import androidx.compose.foundation.layout.PaddingValues
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.navigation.NavHostController
+
+@Composable
+fun CurrentWeatherScreenTEST(innerPadding: PaddingValues) {
+    val tampere=stringResource(R.string.tampere)
+    var weatherResponse by remember { mutableStateOf<WeatherResponse?>(null) }
+    var isLoading by remember { mutableStateOf(true) }
+    var temperature = -999.99
+    var windSpeed = -999.99
+    var windDirection = -999
+    var weatherDescription="Error"
+
+
+    LaunchedEffect(Unit){
+        weatherResponse = RetrofitInstance.apiService.fetchCurrentWeatherTampere()
+        isLoading=false
+    }
+
+    if (weatherResponse != null) {
+        temperature = weatherResponse!!.main.temp
+        windSpeed = weatherResponse!!.wind.speed
+        windDirection = weatherResponse!!.wind.deg
+        weatherDescription = weatherResponse!!.weather[0].main
+    }
+
+    if (isLoading) {
+        CircularProgressIndicator()
+    }
+    else {
+
+        MaterialTheme {
+            Column(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .background(Color.LightGray)
+                , horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                Spacer(Modifier.padding(innerPadding))
+                Row(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(18.dp),
+                    horizontalArrangement = Arrangement.SpaceBetween,
+
+                    verticalAlignment = Alignment.CenterVertically
+                ) {
+                    Text(
+                        text = "$temperature °C",
+                        style = TextStyle(
+                            color = Color(0xFF115C9E),
+                            fontSize = 55.sp,
+                            fontWeight = FontWeight.Bold
+                        ),
+                        modifier = Modifier.padding(end = 8.dp)
+                    )
+
+                    Image(
+                        painter = painterResource(id = R.drawable.sad_document),
+                        contentDescription = "Snowy Weather Icon",
+                        modifier = Modifier
+                            .size(120.dp)
+                    )
+                }
+
+                Text(
+                    text = weatherDescription,
+                    color = Color.Black,
+                    fontSize = 25.sp,
+                    fontWeight = FontWeight.Bold,
+                    textAlign = TextAlign.Center,
+                    modifier = Modifier
+                        .fillMaxWidth()
+                )
+                Spacer(modifier = Modifier.height(10.dp))
+
+                Text("Wind speed: $windSpeed m/s, Direction: $windDirection",
+                    color = Color.Black,
+                    fontSize = 16.sp,
+                    fontWeight = FontWeight.Bold,
+                    textAlign = TextAlign.Center,
+                    modifier = Modifier
+                        .fillMaxWidth()
+                )
+
+                Spacer(modifier = Modifier.weight(1f))
+
+
+            }
+        }
+    }
+}
---
+++ app/src/main/java/com/example/myapplication/TopBar.kt
@@ -0,0 +1,102 @@
+package com.example.myapplication
+
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material.icons.filled.LocationOn
+import androidx.compose.material.icons.filled.Menu
+import androidx.compose.material3.BottomAppBar
+import androidx.compose.material3.BottomAppBarDefaults
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.FloatingActionButtonDefaults
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.material3.rememberTopAppBarState
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.input.nestedscroll.nestedScroll
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.unit.dp
+
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun CenterAlignedTopAppBarExample() {
+    val scrollBehavior = TopAppBarDefaults.pinnedScrollBehavior(rememberTopAppBarState())
+
+    Scaffold(
+        modifier = Modifier.nestedScroll(scrollBehavior.nestedScrollConnection),
+
+        topBar = {
+            CenterAlignedTopAppBar(
+                colors = TopAppBarDefaults.topAppBarColors(
+                    containerColor = MaterialTheme.colorScheme.primaryContainer,
+                    titleContentColor = MaterialTheme.colorScheme.primary,
+                ),
+                title = {
+                    Text(
+                        "Tampere",
+                        maxLines = 1,
+                        overflow = TextOverflow.Ellipsis,
+                        color = Color.Black,
+                    )
+                },
+                navigationIcon = {
+                    IconButton(onClick = { /* do something */ }) {
+                        Icon(
+                            imageVector = Icons.Filled.ArrowBack,
+                            contentDescription = "Localized description"
+                        )
+                    }
+                },
+                actions = {
+                    IconButton(onClick = { /* do something */ }) {
+                        Icon(
+                            imageVector = Icons.Filled.Menu,
+                            contentDescription = "Localized description"
+                        )
+                    }
+                },
+                scrollBehavior = scrollBehavior,
+            )
+        },
+        bottomBar = {
+            BottomAppBar(
+                actions = {
+                    IconButton(onClick = { /* do something */ }) {
+                        Icon(Icons.Filled.LocationOn, contentDescription = "Localized description")
+                    }
+
+                    IconButton(onClick = { /* do something */ }) {
+                        Icon(
+                            painter = painterResource(id = R.drawable.sad_document),
+                            contentDescription = "Localized description",
+                        )
+                    }
+                },
+                floatingActionButton = {
+                    FloatingActionButton(
+                        onClick = { /* do something */ },
+                        containerColor = BottomAppBarDefaults.bottomAppBarFabColor,
+                        elevation = FloatingActionButtonDefaults.bottomAppBarFabElevation()
+                    ) {
+                        Icon(painter = painterResource(id = R.drawable.chain_link_icon), contentDescription ="Localized description",modifier = Modifier.size(20.dp))
+                    }
+                }
+            )
+        },
+
+        ) { innerPadding ->
+        CurrentWeatherScreenTEST(innerPadding)
+    }
+}
+
+
